1f7d633565bf200a6fce72ac32cdd8bd920ee071
huaxu <tshy0425@hotmail.com>
2020-04-07 15:13:47 +0800

    add part leetcode solution 200,207

---
+++ leetCode/topic200.go
@@ -0,0 +1,34 @@
+package leetCode
+
+var dx = [4]int{-1, 1, 0, 0}
+var dy = [4]int{0, 0, 1, -1}
+
+func numIslands(grid [][]byte) int {
+	m := len(grid)
+	if m == 0 {
+		return 0
+	}
+
+	n := len(grid[0])
+	count := 0
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == '1' {
+				DFS(grid, i, j)
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func DFS(grid [][]byte, i, j int) {
+	if i < 0 || j < 0 || i >= len(grid) || j >= len(grid[0]) || grid[i][j] != '1' {
+		return
+	}
+
+	grid[i][j] = '0'
+	for k := 0; k < 4; k++ {
+		DFS(grid, i+dx[k], j+dy[k])
+	}
+}
